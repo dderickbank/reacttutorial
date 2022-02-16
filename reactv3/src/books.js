3b7ac2508642eeb0bbbd52348eeec3d8e3506df3
derick bank <derick.bank@integrify.io>
2022-02-16 14:10:33 +0100

    new upload

---
+++ reactv3/src/books.js
@@ -0,0 +1,21 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGbZ53VqO8uGnaGRd2aiLvuthRYOA2p28S0MYUviCL1SA1Mq9mrFHvAl-abNRaUTB46R0&usqp=CAU",
+    title: "Keep them poor - rich dad poor dad - cashflow quadrant",
+    author: "Robert Kiyosaki",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT24-sqggtXAurzg57IwxrLtOGfQFFP5vAneSuyAQ7v6-1tQ68ufY5Ha5GUYQ0XqyWiX6U&usqp=CAU",
+    title: "The art of not giving a fuck",
+    author: "derick banks",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/41kbdfeipVS._SX344_BO1,204,203,200_.jpg",
+    title: "The compound effect",
+    author: "Darren Hardy",
+  },
+
+];
---
+++ reactv3/testing.js
@@ -0,0 +1,10 @@
+// const arr = ["fish" ,"chips" ,"cocaCola", "jfdkfdk", "fdjhfdjhf", "dfjhdjfj" ]
+
+
+
+
+// // Destructured:
+// const [fish, chips, banana, fruit, sorting, chicken] = arr;
+// const [cocaCola,...rest] = arr;
+// console.log(cocaCola, ...rest);
+
